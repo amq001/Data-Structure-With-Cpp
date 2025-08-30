94d2592db4f4ca786aee895cf6ebab064bc94f63
amq001 <aqibmehtab001@gmail.com>
2025-08-30 20:39:21 +0500

    basics

---
+++ 01_Basics/01_Flowcharts_PsuedoCode/01_Sum_Of_Two_Number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// 1) Sum of two numbers
+int main()
+{
+  int a, b, sum;
+  cout << "Enter two numbers:" << endl;
+  cin >> a >> b;
+  sum = a + b;
+  cout << "Sum of " << a << " and " << b << " is " << sum << endl;
+}
